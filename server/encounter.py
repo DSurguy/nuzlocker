d0acd99919073cb668b018c0ae3432de95afbe84
Agelb <ajgelber@gmail.com>
2018-02-06 21:13:04 -0600

    Basic flask framework

---
+++ server/encounter.py
@@ -0,0 +1,6 @@
+class Encounter:
+
+    def __init__(self, time, route, pokemonIds):
+        self.time = time
+        self.route = route
+        self.pokemonIds = pokemonIds
---
+++ server/nuzlocker.py
@@ -0,0 +1,23 @@
+import datetime
+
+from flask import Flask, request
+from encounter import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/pokemon/<pokemonId>")
+def pokemonInfo(pokemonId):
+    return "Information on %s" % 'bulbasaur'
+
+@app.route("/encounter", methods=['POST'])
+def postEncounter():
+    if request.method == 'POST':
+        data = request.get_json()
+        pokemonIds = data['pokemonIds']
+        route = data['route']
+        e = Encounter(datetime.datetime.now().time(), route, pokemonIds)
+        return "Logged encounter with {0} on route {1}".format(pokemonIds, route)
